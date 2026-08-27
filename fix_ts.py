d9573b84aa7c861019edad4ea37c58da40742359
VenezuelanBiggie24 <309613215+VenezuelanBiggie24@users.noreply.github.com>
2026-08-27 02:31:58 -0400

    chore: remove temporary build scripts

--- fix_ts.py
+++
@@ -1,7 +0,0 @@
-with open("src/App.tsx", "r") as f:
-    content = f.read()
-
-content = content.replace("Loader2, ", "")
-
-with open("src/App.tsx", "w") as f:
-    f.write(content)
--- test_rust.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    let _: fs_extra::dir::TransitProcessResult = fs_extra::dir::TransitProcessResult::ContinueOrAbort;
-}
